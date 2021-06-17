4b37321e7b34fcba70667171262623a7e1899815
Fernanda Araujo <nanda.arf@gmail.com>
2021-06-17 19:07:19 -0300

    Initial Commit

---
+++ Shared/ChuckNorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisApp.swift
+//  Shared
+//
+//  Created by Fernanda Araujo on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
