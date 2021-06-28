78430cd6992b0d57208e949a80bd89d04157588b
Rafaela Lourenço <rafaela.lourenco@thoughtworks.com>
2021-06-28 12:26:40 -0300

    creates a ContentView to run application

---
+++ App/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Agora Vai!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
